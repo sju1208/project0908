fd6986beff0ac1cf3f5699f20251a46a76547c10
yj-603-11 <yj-603-11@DESKTOP-8VREOB1>
2022-09-08 10:22:13 +0900

    project0908 두번째

--- src/project0908/Exam01.java
+++ src/project0908/Exam01.java
@@ -1,10 +1,11 @@
 package project0908;
 
-public class Hello {
+public class Exam01 {
 
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
 		System.out.println("헬로월드");
+
 	}
 
 }
